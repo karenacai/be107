1314417a0f488d7f4af1788b70b4dbdc12e16157
karena <kcai@caltec.edu>
2019-03-27 18:27:10 -0700

    camera stuff

---
+++ testCalibration/analyzeTracks.py
@@ -0,0 +1,22 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+f = h5py.File("data.h5","r")
+
+dset = f['r_vecs']
+
+x = []
+y = []
+z = []
+for a in dset:
+    x+=[a[0]]
+    y+=[a[1]]
+    z+=[a[2]]
+    
+fig = plt.figure()
+ax = Axes3D(fig)
+ax.scatter(x,y,z)
+
+plt.show()
