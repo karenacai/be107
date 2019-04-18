c832ee2a829693ea341788b8f3e667d27a8d6452
dr3y <ashur@caltech.edu>
2019-04-18 14:06:19 -0700

    added raspberry pi camera code to cam_publisher

--- Lab3/image_publisher/nodes/cam_publisher.py
+++ Lab3/image_publisher/nodes/cam_publisher.py
@@ -4,36 +4,70 @@ import cv2
 import sys
 import numpy as np
 from sensor_msgs.msg import Image
-from cv_bridge import CvBridge, CvBridgeError 
+from cv_bridge import CvBridge, CvBridgeError
 import time
-
+import socket
+#we're going to label our topic with our hostname
+#so we don't get confused between all the other robots
+#that will be publishing their images
+hostname = socket.gethostname()
 # set up publisher node to take camera input and output to ROS camera topic
-pub = rospy.Publisher('/camera/image', Image, queue_size=100)
+pub = rospy.Publisher('/{}/image'.format(hostname), Image, queue_size=100)
 rospy.init_node('image_publisher', anonymous=True)
 bridge = CvBridge()
 
-# set up subscriber node to read from ROS camera topic, do some preliminary image processing and view image data
 
-dowebcam=False
-# wrapper (use camera capture, otherwise use webcam for feed)
+cam = None
+#this should work for publishing images from a webcam or from
+#the firefly camera or from the raspberry pi camera.
 try:
+    #first let's see if we have a point grey firefly camera
     import cam_capture
+    cam = cam_capture.FlyCamera(0)
 except ModuleNotFoundError:
-    dowebcam = True
-
-if(dowebcam):
-    cam = cv2.VideoCapture(0)
-else:
-    cam = cam_capture.FlyCamera(0) #intialize the pointgrey camera
-
+    #if we don't have a point grey firefly camera then this will
+    #trigger.
+    try:
+        import picamera
+        #we're going to define a new class in this try except block
+        #to make the picamera behave the same as a default camera from
+        #opencv's perspective
+        class dummyCam:
+            def __init__(self,camera,res=(640,480),framerate=32):
+                self.cam = camera
+                camera.resolution = res
+                camera.framerate = framerate
+                #we need a stream to get the image data. Not sure why but
+                #this works the fastest
+                self.stream = io.BytesIO()
+            def read(self):
+                #this part captures an image and adds it to the stream
+                self.cam.capture(self.stream, format='jpeg', use_video_port=True)
+                #now, we convert the stream into numpy data. First we get
+                #the stream data into an array, then decode that array into a
+                #different array of RGB values. Actually the values are BGR
+                data = np.fromstring(self.stream.getvalue(), dtype=np.uint8)
+                img = cv2.imdecode(data, 1)
+                #this next part removes the current image from the stream
+                #to prepare it for the next image.
+                stream.seek(0)
+                stream.truncate()
+                return True, img
+            def __del__(self):
+                #this part runs when this class is destroyed. This will ensure
+                #that our program does not hog the camera for ever and ever
+                self.cam.close()
+        #after defining the class we initialize the class.
+        cam = dummyCam(picamera.PiCamera())
+    except ModuleNotFoundError:
+        #finally if neither of those two things worked, then use
+        #the webcam!
+        cam = cv2.VideoCapture(0)
 while(True):
     ret1, origimg = cam.read()
     img = origimg.copy()
-    #gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    #cv2.imshow('image',img)
-    #if cv2.waitKey(1) & 0xFF==ord('q'):
-        #break
+
     try:
         pub.publish(bridge.cv2_to_imgmsg(img))
-    except CvBridgeError as e: 
+    except CvBridgeError as e:
         print(e)
