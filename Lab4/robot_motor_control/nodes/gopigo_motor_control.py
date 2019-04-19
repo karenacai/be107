2bd3a1a313124458109946afce927e77ce665981
dr3y <ashur@caltech.edu>
2019-04-18 22:57:31 -0700

    integer division!

--- Lab4/robot_motor_control/nodes/gopigo_motor_control.py
+++ Lab4/robot_motor_control/nodes/gopigo_motor_control.py
@@ -39,7 +39,7 @@ class Robot:
             speed = 0
         #now we are actually scaling the speed to what we said
         #the maximum speed was going to be.
-        scaledspeed = int((speed/255)*self.maxmot1)
+        scaledspeed = int((float(speed)/255)*self.maxmot1)
         print("scaled speed is {}".format(scaledspeed))
         absspeed = abs(scaledspeed)
         if(self.gpg==None):
