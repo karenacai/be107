9e3c8d78bee96ef58862c07f0beaf281f2599a51
dr3y <ashur@caltech.edu>
2019-05-10 17:09:52 -0700

    gh

--- MLX90333_interface/read_mlx.py
+++ MLX90333_interface/read_mlx.py
@@ -11,7 +11,7 @@ class mlxSensor:
         rdata = self.spicom.xfer([255,255,255,255,255,255,255,255],self.rate,self.delay,self.bits)
         alpha = int((rdata[1]<<8)+rdata[2])
         beta = int((rdata[3]<<8)+rdata[4])
-        return alpha,beta
+        return [alpha,beta]
     def __del__(self):
         self.spicom.close()
 if(__name__=="__main__"):
