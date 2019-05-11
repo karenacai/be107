70a527f8b221a0f00a578169dd2230f23eeba438
dr3y <ashur@caltech.edu>
2019-05-10 17:15:35 -0700

    debug

--- MLX90333_interface/read_mlx.py
+++ MLX90333_interface/read_mlx.py
@@ -12,10 +12,18 @@ class mlxSensor:
         alpha = int((rdata[1]<<8)+rdata[2])
         beta = int((rdata[3]<<8)+rdata[4])
         return alpha,beta
+    def getRawData(self):
+        rdata = self.spicom.xfer([255,255,255,255,255,255,255,255],self.rate,self.delay,self.bits)
+        return rdata
     def __del__(self):
         self.spicom.close()
 if(__name__=="__main__"):
     mymlx = mlxSensor()
     while(True):
-        alpha,beta = mymlx.getData()
+        #alpha,beta = mymlx.getData()
+        #print("alpha {} beta {}".format(alpha,beta))
+        rawdata = mymlx.getRawData()
+        alpha = int((rawdata[1]<<8)+rawdata[2])
+        beta = int((rawdata[3]<<8)+rawdata[4])
         print("alpha {} beta {}".format(alpha,beta))
+        print("alpha {},{} beta {},{}".format(rawdata[1],rawdata[2],rawdata[3],rawdata[4]))
